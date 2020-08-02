29c6f86804730f43c2981053260ebb26a16c6c35
dzendzen <dzen@hotmail.fr>
2020-08-02 09:30:31 +0200

    loading,featured rooms logic

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import { RoomContext } from "../Context";
+import Loading from "./Loading";
+import Room from "./Room";
+import Title from "./Title";
 
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
@@ -7,8 +10,19 @@ export default class FeaturedRooms extends Component {
     // const value = this.context;
     // console.log(value);
     // const {  } = this.context;
-    const { featuredRooms: rooms } = this.context;
-    console.log(rooms);
-    return <div>featured rooms</div>;
+    let { loading, featuredRooms: rooms } = this.context;
+    // console.log(rooms);
+    rooms = rooms.map((room) => {
+      return <Room key={room.id} room={room} />;
+    });
+
+    return (
+      <section className="featured-rooms">
+        <Title title="featuredrooms" />
+        <div className="featured-rooms-center">
+          {loading ? <Loading /> : rooms}
+        </div>
+      </section>
+    );
   }
 }
